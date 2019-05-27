d36c401427d003a8d893e198fbe6274e8b6c2e5f
Douglas Urner <dlu@canishe.com>
2019-05-27 16:36:05 -0700

    Start button script.

---
+++ UI System/Assets/Scripts/StartButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButton : MonoBehaviour
+{
+    // Called when button is clicked.
+    public void OnClick()
+    {
+        Debug.Log(gameObject.name + ".OnClick()");
+        // Load first scene.
+    }
+}
